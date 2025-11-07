018ba7c635ace87e8cd64949277c745ff3203fe0
Awfp1314 <2910385881@qq.com>
2025-11-07 18:14:08 +0800

    Fix clients __init__.py: export create_llm_client and all client classes

--- modules/ai_assistant/clients/__init__.py
+++ modules/ai_assistant/clients/__init__.py
@@ -4,3 +4,18 @@
 客户端模块
 与外部服务（如虚幻引擎编辑器）进行通信
 """
+
+from .llm_client_factory import create_llm_client, create_llm_client_from_config_manager
+from .base_llm_client import BaseLLMClient
+from .api_llm_client import ApiLLMClient
+from .ollama_llm_client import OllamaLLMClient
+from .ue_tool_client import UEToolClient
+
+__all__ = [
+    'create_llm_client',
+    'create_llm_client_from_config_manager',
+    'BaseLLMClient',
+    'ApiLLMClient',
+    'OllamaLLMClient',
+    'UEToolClient'
+]
